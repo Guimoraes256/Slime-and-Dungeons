d3acc78ff3a5344c9437d9ab308247178817eedd
Guilherme Moraes <guimoraes256@gmail.com>
2018-03-13 22:39:55 -0300

    commiT

---
+++ src/main/java/com/slime/and/dungeons/Slime/And/Dungeons/SlimeAndDungeonsApplication.java
@@ -0,0 +1,12 @@
+package com.slime.and.dungeons.Slime.And.Dungeons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlimeAndDungeonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlimeAndDungeonsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/slime/and/dungeons/Slime/And/Dungeons/SlimeAndDungeonsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.slime.and.dungeons.Slime.And.Dungeons;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SlimeAndDungeonsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
